41c8934968ce5fec985755619148f6836e04ebf7
Your Name <you@example.com>
2016-04-18 22:21:13 +0800

    .

---
+++ src/Repetition.java
@@ -0,0 +1,22 @@
+package com.java.ch03;
+public class Repetition{
+  public static void main(String[] args){
+
+
+int arr[]={21,23,312,34};
+for(int i:arr){
+
+System.out.println(i+"    ");
+
+}
+
+
+
+
+
+
+
+
+
+   }
+}
